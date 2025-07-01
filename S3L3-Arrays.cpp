b523acb9a2ae5ad5ab6f592b85c056987189aade
HardikJ-26 <hardik.jain.2210@gmail.com>
2025-07-01 22:45:37 +0530

    Second commit

--- S3L3-Arrays.cpp
+++ S3L3-Arrays.cpp
@@ -169,3 +169,114 @@ int main()
     return 0;
 }
 */
+
+//Q2 MAJORITY ELEMENT(>N/3)
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>findmaj(vector<int>v,int n)
+{
+int cnt1=0,cnt2=0;
+int el1=INT16_MIN,el2=INT16_MIN;
+for(int i=0;i<n;i++)
+{
+    if(cnt1==0 && v[i]!=el2)
+    {
+        cnt1=1;
+        el1=v[i];
+    }
+    if(cnt2==0 && v[i]!=el1)
+    {
+        cnt2=1;
+        el2=v[i];
+    }
+    else if(v[i]==el1)
+    cnt1++;
+    else if(v[i]==el2)
+    cnt2++;
+    else
+    {
+        cnt1--;
+        cnt2--;
+    }
+}
+vector<int>ls;
+cnt1=0,cnt2=0;
+for(int i=0;i<n;i++)
+{
+    {
+        if (v[i] == el1) cnt1++;
+        if (v[i] == el2) cnt2++;
+    }
+}
+int maj=(int)(n/3)+1;
+if (cnt1 >= maj) ls.push_back(el1);
+if (cnt2 >= maj) ls.push_back(el2);
+return ls;
+}
+int main()
+{
+    vector<int> arr = {11, 33, 33, 11, 33, 11};
+    int n=arr.size();
+    vector<int> ans = findmaj(arr,n);
+    cout << "The majority elements are: ";
+    for (auto it : ans)
+        cout << it << " ";
+    cout << "\n";
+    return 0;
+}
+*/
+
+//3 SUM PROBLEM
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>>tsum(vector<int>v,int n)
+{
+    vector<vector<int>>ans;
+    sort(v.begin(),v.end());
+    for(int i=0;i<n-2;i++)
+    {
+      if(i>0 && v[i]==v[i-1])
+      continue;
+        int j=i+1;
+        int k=n-1;
+        while(j<k)
+        {
+           int sum=v[i]+v[j]+v[k];
+        if(sum<0)
+        j++;
+        else if(sum>0)
+        k--;
+        else
+        {
+            vector<int>temp={v[i],v[j],v[k]};
+            ans.push_back(temp); //pushing for clubbibg as matrix
+            j++;
+            k--;
+            //skipping duplicates
+            while (j < k && v[j] == v[j - 1]) j++;
+            while (j < k && v[k] == v[k + 1]) k--;
+        }
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> arr = { -1, 0, 1, 2, -1, -4};
+    int n = arr.size();
+    vector<vector<int>> ans = tsum(arr,n);
+    for (auto it : ans) {
+        cout << "[";
+        for (auto i : it) {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+return 0;
+}
+*/
